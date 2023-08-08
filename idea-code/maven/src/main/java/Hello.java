e65aaaa7f58e872a130841518d065f8c1b07f2bf
Zhaozy <73604892+021116@users.noreply.github.com>
2023-08-08 12:02:29 +0800

    远方的朋友我很好

--- idea-code/maven/src/main/java/Hello.java
+++ idea-code/maven/src/main/java/Hello.java
@@ -2,6 +2,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello world");
         System.out.println("远方的朋友，你好吗？？");
+        System.out.println("远方的朋友，我很好");
     }
 }
 
