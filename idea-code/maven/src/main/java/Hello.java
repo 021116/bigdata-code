30da5d2d9f4d089387bc27fb9b7337eb62191588
Zhaozy <73604892+021116@users.noreply.github.com>
2023-08-08 14:28:31 +0800

    再见了我的朋友

--- idea-code/maven/src/main/java/Hello.java
+++ idea-code/maven/src/main/java/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
         System.out.println("远方的朋友，你好吗？？");
         System.out.println("远方的朋友，我很好");
         System.out.println("再见了我的朋友");
+        System.out.println("你在干什么我的朋友");
     }
 }
 
