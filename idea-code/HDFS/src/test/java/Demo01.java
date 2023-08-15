b11db328a81ff2e6ae304da10ceebb924c64e327
Zhaozy <73604892+021116@users.noreply.github.com>
2023-08-15 23:37:10 +0800

    test02

--- idea-code/HDFS/src/test/java/Demo01.java
+++ idea-code/HDFS/src/test/java/Demo01.java
@@ -3,6 +3,7 @@ import org.apache.hadoop.fs.FileSystem;
 import org.junit.Test;
 
 import java.io.IOException;
+import java.net.URI;
 
 public class Demo01 {
     @Test
@@ -12,6 +13,25 @@ public class Demo01 {
         // HDFS的连接地址
         configuration.set("fs.defaultFS","hdfs://bigdata01:9820");
         FileSystem fileSystem = FileSystem.get(configuration);
+    }
+    @Test
+    public void test02() throws Exception {
+        //  URL 和   URL
+        //  URL =   中华人民共和国
+        //  指的是互联网上比较具体的第一个内容,地址
+        //  URI =   共和国 员工的编号9527
+        //  cookie  session
+        FileSystem fileSystem = FileSystem.get(new URI("hdfs://bigdata01:9820"), new Configuration());
+        System.out.println(fileSystem);
+    }
+    @Test
+    public void test03() throws Exception{
+        // 配置的意思
+        Configuration configuration = new Configuration();
+        //  hdfs的连接地址
+        configuration.set("fs.defaultFS","hdfs://bigdata:9820");
+        FileSystem fileSystem = FileSystem.newInstance(configuration);
+        System.out.println(fileSystem);
     }
 
 }
