35cc53b076d888d03e85b6b40890014eee232e42
Zhaozy <73604892+021116@users.noreply.github.com>
2023-08-15 20:55:52 +0800

    我是结束代码

--- idea-code/HDFS/src/test/java/TestUnit.java
+++ idea-code/HDFS/src/test/java/TestUnit.java
@@ -1,3 +1,4 @@
+import org.junit.After;
 import org.junit.Before;
 
 public class TestUnit {
@@ -5,5 +6,9 @@ public class TestUnit {
     @Before // 每次单元测试方法执行前 都会执行该方法 该方法一般存储一些初始化的代码
     public void init(){
         System.out.println("我是开始代码");
+    }
+    @After  // 每次单元测试方法执行后都会执行该方法 该方法一般都是存放一些连接关闭等收尾工作
+    public void destory(){
+        System.out.println("我是结束代码");
     }
 }
