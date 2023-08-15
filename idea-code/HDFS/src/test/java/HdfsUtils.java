07c120628d395d76479b4d12264a2de6ff1043f4
Zhaozy <73604892+021116@users.noreply.github.com>
2023-08-16 00:37:52 +0800

    testUploadDile

--- idea-code/HDFS/src/test/java/HdfsUtils.java
+++ idea-code/HDFS/src/test/java/HdfsUtils.java
@@ -1,7 +1,9 @@
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 
 public class HdfsUtils {
     private FileSystem fileSystem;
@@ -18,5 +20,11 @@ public class HdfsUtils {
     public void tearDown() throws Exception {
         //  在每个测试方法之后执行的清理工作
         fileSystem.close();
+    }
+    @Test
+    public void testUploadDile() throws Exception{
+        String localFilePath = "C:\\Users\\mortal\\Desktopedip";
+        String hdfsFilePath = "/";
+        fileSystem.copyFromLocalFile(new Path(localFilePath),new Path(hdfsFilePath));
     }
 }
