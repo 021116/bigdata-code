8140b1ef8f0f226e80d6363f701645fdf1da393f
Zhaozy <73604892+021116@users.noreply.github.com>
2023-08-15 23:23:22 +0800

    test01

--- idea-code/HDFS/src/test/java/Demo01.java
+++ idea-code/HDFS/src/test/java/Demo01.java
@@ -1,2 +1,17 @@
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.junit.Test;
+
+import java.io.IOException;
+
 public class Demo01 {
+    @Test
+    public void test01() throws IOException {
+        // 配置的意思
+        Configuration configuration = new Configuration();
+        // HDFS的连接地址
+        configuration.set("fs.defaultFS","hdfs://bigdata01:9820");
+        FileSystem fileSystem = FileSystem.get(configuration);
+    }
+
 }
